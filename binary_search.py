8809913a6fff5d73fd3c797cf8914746bc3cba28
Tomi <52747351+tomiiha@users.noreply.github.com>
2021-03-24 09:59:38 +0000

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,22 +1,31 @@
 def bin_search(data, target):
+    # Set array ends.
     first = 0
-    last = len(ordered_set) - 1
-
+    last = len(data) - 1
+    
+    # Find the rough middle of the array.
     while first <= last:
         middle = (first + last) // 2
-
+        
+        # Check if the initial array placement is the value.
         if data[middle] == target:
             return middle
+        
+        # If the value is larger than the initial place:
+        # Shift array up to the value after the middle, and search remainder array.
         elif data[middle] > target:
             last = middle - 1
+        
+        # Otherwise shift the array beginning up, and search the remainder.
         else:
             first = middle + 1
 
-    return -1
+    return data.index(target)
 
-# Set of ordered data to check
-
+# Set of ordered data to check.
 ordered_set = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 # Target value aimed at finding.
-ex_target = 67
+target = 67
+
+# Run search for target in array.
 bin_search(primes, ex_target)
