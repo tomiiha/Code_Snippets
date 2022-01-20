795966e53d803d753f1190a216c688ce37e7f751
Tomi <52747351+tomiiha@users.noreply.github.com>
2022-01-20 19:56:19 +0000

    Update logistic_regression.py

--- ML/logistic_regression.py
+++ ML/logistic_regression.py
@@ -1,14 +1,13 @@
-
 '''
 Create df, with 'A' column as category, the rest as random ints.
 Model will be terrible - this is a proof of concept on code.
 '''
 def rand_df():
-  import pandas as pd
-  import numpy as np
-  df = pd.DataFrame(np.random.randint(0,3,size=(500, 1)), columns=list('A'))
-  df = df.join(pd.DataFrame(np.random.randint(0,50,size=(500, 3)), columns=list('BCD')))
-  return df
+    import pandas as pd
+    import numpy as np
+    df = pd.DataFrame(np.random.randint(0,3,size=(500, 1)), columns=list('A'))
+    df = df.join(pd.DataFrame(np.random.randint(0,50,size=(500, 3)), columns=list('BCD')))
+    return df
 
 def log_reg(df = rand_df()):
     from sklearn.linear_model import LogisticRegression
