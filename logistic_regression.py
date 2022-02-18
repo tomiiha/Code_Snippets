62d1ec69298b403bc33344b1824d7d56c900a745
Tomi <52747351+tomiiha@users.noreply.github.com>
2022-02-18 12:55:16 +0000

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,31 +1,22 @@
-'''
-Create df, with 'A' column as category, the rest as random ints.
-Model will be terrible - this is a proof of concept on code.
-'''
-def rand_df():
-    import pandas as pd
-    import numpy as np
-    df = pd.DataFrame(np.random.randint(0,3,size=(500, 1)), columns=list('A'))
-    df = df.join(pd.DataFrame(np.random.randint(0,50,size=(500, 3)), columns=list('BCD')))
-    return df
-
-def log_reg(df = rand_df()):
+def log_reg(samples = 500, feats = 5, classes = 2):
     from sklearn.linear_model import LogisticRegression
+    from sklearn.datasets import make_classification
     from sklearn.model_selection import train_test_split
     from sklearn import metrics
+    import pandas as pd
+    import numpy as np
+    
+    # Create classification dummy dataset.
+    X, y = make_classification(n_samples=samples,
+                               n_features=feats,
+                               n_classes=classes,
+                               random_state = 21)
 
     # Instanteate model class.
     logreg = LogisticRegression()
 
-    # Capture columns (X) ...
-    feature_cols = ['B','C','D']
-    X = df[feature_cols]
-
-    # Used to predict value (y).
-    y = df.A
-
     # Train/Test Split.
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=99)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=21)
 
     # Fit the Model.
     logreg.fit(X,y)
