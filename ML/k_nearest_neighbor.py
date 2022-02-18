dde64917c3f3cf3ed2a5c500f00c59d11e07c309
Tomi <52747351+tomiiha@users.noreply.github.com>
2022-02-18 12:50:09 +0000

    Update k_nearest_neighbor.py

--- ML/k_nearest_neighbor.py
+++ ML/k_nearest_neighbor.py
@@ -1,32 +1,26 @@
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-import pandas as pd
-import numpy as np
-
-'''
-Create df, with 'A' column as category, the rest as random ints.
-Model will be terrible - this is a proof of concept on code.
-'''
-def rand_df():
-    df = pd.DataFrame(np.random.randint(0,3,size=(500, 1)), columns=list('A'))
-    df = df.join(pd.DataFrame(np.random.randint(0,50,size=(500, 3)), columns=list('BCD')))
-    return df
+def best_knn(samples = 500, feats = 5, classes = 2):
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.datasets import make_classification
+    from sklearn.model_selection import train_test_split
+    from sklearn import metrics
+    import pandas as pd
+    import numpy as np
     
-def best_knn(df=rand_df()):
+    # Create classification dummy dataset.
+    X, y = make_classification(n_samples=samples, 
+                             n_features=feats,
+                             n_classes=classes,
+                             random_state = 21)
+        
     # Create a for loop to calculate the TRAINING ERROR and TESTING ERROR for K=1 through the square root of df.
-
-    n_sqr = int(np.sqrt(len(df)))
+    n_sqr = int(np.sqrt(samples))
     k_range = list(range(1, n_sqr + 1))
     training_error = []
     testing_error = []
 
     # Find test accuracy for all values of K between 1 and square root of n (inclusive).
     for k in k_range:
-        feats = ['B','C']
         knn = KNeighborsClassifier(n_neighbors=k)
-        X = df[feats]
-        y = df.A
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=99)
         knn.fit(X_train, y_train)
 
